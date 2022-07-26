361140bea2d17c3038a6d3a74bfc240b528f6199
duasong111 <98256947+duasong111@users.noreply.github.com>
2022-07-26 17:14:28 +0800

    Create fputc的使用.c

---
+++ C语言/文件操作/fputc的使用.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+int main()
+{
+FILE* pf=fopen("Example.txt", "w");
+	if (pf == NULL)
+	{
+		//文件失败
+		printf("%s\n", strerror(errno)); //#include <string.h>和#include <errno.h>对应里边俩
+		return 0;
+	}
+	fputc('A', pf); //写 
+	//但是一次只是读一个
+	fclose(pf);//值传递
+	pf == NULL;
+return 0;
+}
